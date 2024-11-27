25c1e66483bea1c88c7437b8ff43f25ce6388912
Minye Zhang <minyez.physchem@gmail.com>
2024-11-27 13:26:21 +0100

    add variables to tract atom types

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -2,3 +2,5 @@
 
 std::map<atom_t, coord_t> coord;
 std::map<atom_t, coord_t> coord_frac;
+
+std::map<atom_t, int> atom_types;
--- src/geometry.h
+++ src/geometry.h
@@ -4,5 +4,12 @@
 #include "atoms.h"
 
 typedef std::array<double, 3> coord_t;
+
+//! Cartesian coordinates of atoms
 extern std::map<atom_t, coord_t> coord;
+
+//! Fractional coordinates of atoms
 extern std::map<atom_t, coord_t> coord_frac;
+
+//! Atom type of atoms
+extern std::map<atom_t, int> atom_types;
