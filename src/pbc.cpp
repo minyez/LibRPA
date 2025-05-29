0b079bca61972de3925c4d4b0e671ba7e419fc38
Minye Zhang <minyez.physchem@gmail.com>
2025-05-29 17:58:01 +0200

    refact: allow generating R grid in upper half

--- src/pbc.cpp
+++ src/pbc.cpp
@@ -1,16 +1,26 @@
 #include "pbc.h"
 #include <algorithm>
 
-vector<Vector3_Order<int>> construct_R_grid(const Vector3_Order<int> &period)
+vector<Vector3_Order<int>> construct_R_grid(const Vector3_Order<int> &period, bool upper_half)
 {
     // cout<<" begin to construct_R_grid"<<endl;
     vector<Vector3_Order<int>> R_grid;
     R_grid.clear();
 
-    for (int x = -(period.x) / 2; x <= (period.x - 1) / 2; ++x)
-        for (int y = -(period.y) / 2; y <= (period.y - 1) / 2; ++y)
-            for (int z = -(period.z) / 2; z <= (period.z - 1) / 2; ++z)
-                R_grid.push_back({x, y, z});
+    if (upper_half)
+    {
+        for (int x = -(period.x - 1) / 2; x <= period.x / 2; ++x)
+            for (int y = -(period.y - 1) / 2; y <= period.y / 2; ++y)
+                for (int z = -(period.z - 1) / 2; z <= period.z / 2; ++z)
+                    R_grid.push_back({x, y, z});
+    }
+    else
+    {
+        for (int x = -(period.x) / 2; x <= (period.x - 1) / 2; ++x)
+            for (int y = -(period.y) / 2; y <= (period.y - 1) / 2; ++y)
+                for (int z = -(period.z) / 2; z <= (period.z - 1) / 2; ++z)
+                    R_grid.push_back({x, y, z});
+    }
 
     return R_grid;
 }
