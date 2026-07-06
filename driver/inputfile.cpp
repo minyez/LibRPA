29e7511bbb368f4cb440d5e5d76dc329f8438106
Minye Zhang <minyez.physchem@gmail.com>
2026-07-06 20:19:10 +0800

    refactor: separate input_parser

--- driver/inputfile.cpp
+++ driver/inputfile.cpp
@@ -1,131 +1,20 @@
 #include "inputfile.h"
 
-#include <regex>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <cstring>
-
-#include "driver.h"
-
-#include "librpa_enums.h"
-#include "../src/io/fs.h"
-#include "../src/utils/constants.h"
-#include "../src/utils/dev_options.h"
 #include <regex>
 #include <sstream>
 #include <limits>
 
 #include "driver.h"
 
-static const std::string SPACE_SEP = "[ \r\f\t]*";
+#include <librpa_enums.h>
 
-const std::string InputParser::KV_SEP = "=";
-const std::string InputParser::COMMENTS_IDEN = "[#!]";
-
-static std::string get_last_matched(const std::string &s, const std::string &key,
-                                    const std::string &vregex, int igroup)
-{
-    std::string sout = "";
-    std::regex r("(^|[\r\n])" + SPACE_SEP + key + SPACE_SEP
-                 + InputParser::KV_SEP + SPACE_SEP + vregex,
-                 std::regex_constants::ECMAScript | std::regex_constants::icase);
-    std::sregex_iterator si(s.begin(), s.end(), r);
-    auto ei = std::sregex_iterator();
-    for (auto i = si; i != ei; i++)
-    {
-        std::smatch match = *i;
-        // std::cout << "whole matched string: " << match.str(0) << std::endl;
-        sout = match.str(igroup + 1);
-    }
-    return sout;
-}
-
-void InputParser::parse_double(const std::string &vname, double &var, int &flag) const
-{
-    flag = 0;
-    std::string s = get_last_matched(params, vname, "(-?[\\d]+\\.?([\\d]+)?([ed]-?[\\d]+)?)", 1);
-    if (s != "")
-    {
-        try
-        {
-            var = std::stod(s);
-        }
-        catch (std::invalid_argument const&)
-        {
-            flag = 2;
-        }
-    }
-    else
-        flag = 1;
-}
-
-
-void InputParser::parse_double(const std::string &vname, double &var, double de, int &flag) const
-{
-    parse_double(vname, var, flag);
-    if (flag) var = de;
-}
-
-void InputParser::parse_int(const std::string &vname, int &var, int &flag) const
-{
-    flag = 0;
-    std::string s = get_last_matched(params, vname, "(-?[\\d]+)", 1);
-    if (s != "")
-    {
-        try
-        {
-            var = std::stoi(s);
-        }
-        catch (std::invalid_argument const&)
-        {
-            flag = 2;
-        }
-    }
-    else
-        flag = 1;
-}
-
-void InputParser::parse_int(const std::string &vname, int &var, int de, int &flag) const
-{
-    parse_int(vname, var, flag);
-    if (flag) var = de;
-}
-
-void InputParser::parse_string(const std::string &vname, std::string &var, int &flag) const
-{
-    flag = 0;
-    std::string s = get_last_matched(params, vname, "([\\w\\d_\\- ,:;./]+)", 1);
-    if (s != "")
-        var = s;
-    else
-        flag = 1;
-}
-
-void InputParser::parse_string(const std::string &vname, std::string &var, const std::string &de, int &flag) const
-{
-    parse_string(vname, var, flag);
-    if (flag) var = de;
-}
-
-void InputParser::parse_bool(const std::string &vname, bool &var, int &flag) const
-{
-    flag = 0;
-    std::string s = get_last_matched(params, vname, "([\\w]+)", 1);
-    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-    if (s == "true" || s == "t" || s == ".t.")
-        var = true;
-    else if (s == "f" || s == "false" || s == ".f.")
-        var = false;
-    else
-        flag = 1;
-}
-
-void InputParser::parse_bool(const std::string &vname, bool &var, const bool &de, int &flag) const
-{
-    parse_bool(vname, var, flag);
-    if (flag) var = de;
-}
+#include "../src/io/fs.h"
+#include "../src/utils/constants.h"
+#include "../src/utils/dev_options.h"
 
 InputParser InputFile::load(const std::string &fn, bool error_if_fail_open)
 {
--- driver/inputfile.h
+++ driver/inputfile.h
@@ -1,48 +1,7 @@
 #pragma once
 #include <string>
 
-//! Class to parse parameters from string
-/*!
-  Member functions prefixed with parse_ have a int argument at last.
-  This flag is to indicate whether parsing is succesful.
-  The meaning of return value:
-
-  - 0: succesful
-  - 1: failed to find the parameter string, or the parameter value is empty
-  - 2: find the parameter string, but fail to convert the value to requested type
-
-  When multiple matches are found, the last one will be used.
-
-  Only successful parse will lead to value change of the variable.
-  Otherwise the default value will be used.
- */
-class InputParser
-{
-    public:
-        //! separator between the parameter key and value
-        static const std::string KV_SEP;
-        //! comments identifier to trim
-        static const std::string COMMENTS_IDEN;
-    private:
-        std::string params; //!< string storing all parameters, comments trimed
-    public:
-        InputParser() {};
-        InputParser(const std::string &ps): params(ps) {};
-        std::string get_params() const { return params; };
-        void load(const std::string &ps) { params = ps; };
-        //! parse single double value to var
-        void parse_double(const std::string &vname, double &var, int &flag) const;
-        void parse_double(const std::string &vname, double &var, double de, int &flag) const;
-        //! parse single integer value to var
-        void parse_int(const std::string &vname, int &var, int &flag) const;
-        void parse_int(const std::string &vname, int &var, int de, int &flag) const;
-        //! parse single string to var
-        void parse_string(const std::string &vname, std::string &var, int &flag) const;
-        void parse_string(const std::string &vname, std::string &var, const std::string &de, int &flag) const;
-        //! parse single bool to var
-        void parse_bool(const std::string &vname, bool &var, int &flag) const;
-        void parse_bool(const std::string &vname, bool &var, const bool &de, int &flag) const;
-};
+#include "input_parser.h"
 
 //! Class to represent the input file
 /*!
@@ -50,14 +9,15 @@ class InputParser
  */
 class InputFile
 {
-    private:
-        std::string filename;
-        std::string orig_content;
-    public:
-        InputFile() {};
-        InputParser load(const std::string &fn, bool error_if_fail_open = false);
-        std::string get_filename() { return filename; };
-        std::string get_orig_content() { return orig_content; };
+private:
+    std::string filename;
+    std::string orig_content;
+
+public:
+    InputFile() {};
+    InputParser load(const std::string& fn, bool error_if_fail_open = false);
+    std::string get_filename() { return filename; };
+    std::string get_orig_content() { return orig_content; };
 };
 
 void parse_inputfile_to_params(const std::string& fn);
